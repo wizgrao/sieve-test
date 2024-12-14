581022eea9a4979f6297bba19ccf91ba833aa42b
Gaurav Rao <wizgrao@gmail.com>
2024-12-13 17:56:48 -0800

    add test

--- test_a/a.py
+++ test_a/a.py
@@ -2,7 +2,7 @@ import sieve
 import time
 from utils.ci import get_function_name, get_function_uri
 
-@sieve.function(name=get_function_name("test-a"))
+@sieve.function(name=get_function_name("test-a"), python_version="3.10")
 def test_a(a: int):
     time.sleep(a)
     return a
--- test_b/b.py
+++ test_b/b.py
@@ -3,6 +3,6 @@ import time
 from utils.ci import get_function_name, get_function_uri
 import os
 
-@sieve.function(name=get_function_name("test-b"), environment_variables=[sieve.Env(name="SIEVE_TEST_ENV", description="test environment", default=os.getenv("SIEVE_TEST_ENV") or "")])
+@sieve.function(name=get_function_name("test-b"), environment_variables=[sieve.Env(name="SIEVE_TEST_ENV", description="test environment", default=os.getenv("SIEVE_TEST_ENV") or "")], python_version="3.10")
 def test_b(a: int):
     return sieve.function.get(get_function_uri("test-a")).run(a)
