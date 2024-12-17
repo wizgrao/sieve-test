16003cbf21c49c3c1740b088ddcb973738fcaf7e
Gaurav Rao <wizgrao@gmail.com>
2024-12-16 19:43:08 -0800

    fix(test-b): use correct org name

--- test_b/b.py
+++ test_b/b.py
@@ -3,4 +3,4 @@ from utils.ci import get_function_name, get_function_uri, get_env_passthrough
 
 @sieve.function(name=get_function_name("test-b"), environment_variables=get_env_passthrough(), python_version="3.10")
 def test_b(a: int):
-    return sieve.function.get(get_function_uri("test-a")).run(a)
+    return sieve.function.get(get_function_uri("test-a", org="sieve-internal")).run(a)
