97b14a2bc6ebc8606fdd1a6dbd6f9f6dddeed739
Gaurav Rao <wizgrao@gmail.com>
2024-12-16 20:12:28 -0800

    fix(test-b): add print statement

--- test_b/b.py
+++ test_b/b.py
@@ -3,4 +3,5 @@ from utils.ci import get_function_name, get_function_uri, get_env_passthrough
 
 @sieve.function(name=get_function_name("test-b"), environment_variables=get_env_passthrough(), python_version="3.10")
 def test_b(a: int):
+    print("hello world")
     return sieve.function.get(get_function_uri("test-a", org="sieve")).run(a)
